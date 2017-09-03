91727c6e33f0c18808d4b3d7e8b7fb99e9ac6b35
Brett <Leasure57@gmail.com>
2017-09-02 21:35:13 -0600

    Changed file name and added comments

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-fileName = "fertility_Diagnosis.txt";
+fileName = "fertility_Diagnosis.txt"; # name of file containing data
 
 def main():
     openFile();
     
+# open file    
 def openFile():
     with open(fileName, encoding = "utf8") as inFile:
         processedFile = processFile(inFile);
         
+# use file contents to build .arff file
 def processFile(inFile):
     for line in inFile:
         line = line.rstrip();
