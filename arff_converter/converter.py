023b69d952e4ac0e5057decde01b0d01a9886f04
Kirby <knoverman@comcast.net>
2017-09-04 10:15:07 -0600

    Added converter and three samples

---
+++ arff_converter/converter.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+
+
+class Converter():
+
+    def __init__(self, data, attributes):
+        self.data_file = data
+        self.attribute_file = attributes
+        self.data = []
+        self.att = []
+
+    def convert(self):
+        self.process_relation()
+        self.process_attributes()
+        self.process_data()
+
+    def process_relation(self):
+        self.arff_relation()
+    
+    def process_attributes(self):
+        with open(self.attribute_file) as f:
+            for line in f:
+                line = line.split(',')
+                self.att.append(line)
+        self.arff_attributes()
+
+    def process_data(self):
+        with open(self.data_file) as f:
+            for line in f:
+                line = line.split(',')
+                self.data.append(line)
+        self.arff_data()
+
+    def arff_relation(self):
+        arff = open('data.arff', 'w')
+        arff.write('@relation goes_here\n\n')
+        arff.close()
+
+    def arff_attributes(self):
+        arff = open('data.arff', 'a')
+        self.trim_attributes()
+        for r in range(len(self.att)):
+            if self.att[r][1] == 'string' or self.att[r][1] == 'numeric':
+                arff.write("@attribute %s %s\n" % (self.att[r][0], self.att[r][1]))
+            else:
+                if self.att[r][1][:1] == '{': # This is a nominal-specification
+                    self.att[r][1].replace('&', ',')
+                    arff.write("@attribute %s %s\n" % (self.att[r][0], self.att[r][1].replace('&', ',')))
+                else: # This is a date
+                    arff.write("@attribute %s %s\n" % (self.att[r][0], self.att[r][1]))
+        arff.write("\n")
+        arff.close()
+
+    def arff_data(self):
+        arff = open('data.arff', 'a')
+        arff.write("@data\n")
+        for r in range(len(self.data)):
+            arff.write(','.join(self.data[r]))
+        arff.close()
+
+    def trim_attributes(self):
+        for r in range(len(self.att)):
+            for c in range(len(self.att[r])):
+                s = list(self.att[r][c])
+                s = [x for x in s if x != '\"']
+                s = [x for x in s if x != '\n']
+                self.att[r][c] = ''.join(s)
+            self.att[r][0] = self.att[r][0].replace(" ", "_")
+
+if __name__ == "__main__":
+    converter = Converter('data.csv', 'attributes.csv')
+    converter.convert()
